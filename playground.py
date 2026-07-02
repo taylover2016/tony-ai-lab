34f9e00eb782ca10009a8cea5336f684267db245
taylover2016 <taylover2016@yahoo.com>
2026-07-03 00:59:37 +0800

    I have learned Pydantic!

--- playground.py
+++ playground.py
@@ -1,19 +1,29 @@
 from dataclasses import dataclass
+from pydantic import BaseModel
 
-@dataclass
-class Student:
+class Student(BaseModel):
     name: str
     age: int
 
 def main() -> None:
-    leah = Student("Leah", 24)
+    leah = Student(name="Leah", age=24)
     print(f"Student: {leah.name}, Age: {leah.age}")
+    print(type(leah.age))
 
-    tony = Student("Tony", 26)
+    tony = Student(name="Tony", age=26)
     print(f"Student: {tony.name}, Age: {tony.age}")
 
     print(tony==leah)
-    print(tony==Student("Tony", 26))
+    print(tony==Student(name="Tony", age=26))
+
+    print(tony.model_dump())
+    print(type(tony.model_dump()))
+    print(Student.model_validate(tony.model_dump()))
+
+    print(tony.model_dump_json())
+    print(type(tony.model_dump_json()))
+    print(Student.model_validate_json(tony.model_dump_json()))
+
 
 if __name__ == "__main__":
     main()
